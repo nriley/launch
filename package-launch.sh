c3f6d6ea45f33a944c23de8160327e54984b9293
Nicholas Riley <launchsw@sabi.net>
2006-07-20 11:49:13 +0000

    package-launch.sh, launch/README: New, simpler packaging process.

--- package-launch.sh
+++ package-launch.sh
@@ -4,33 +4,18 @@ set -x -v
 
 cd launch && \
 find . -name \*~ -exec rm '{}' \; && \
-xcodebuild -buildstyle Deployment && \
+xcodebuild -configuration Deployment clean && \
+xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install && \
 SetFile -c 'R*ch' -t 'TEXT' README VERSION launch.1 && \
-strip build/launch && \
-sudo /usr/bin/install -c build/launch /usr/local/bin && \
+sudo /usr/bin/install -c launch /usr/local/bin && \
 sudo /usr/bin/install -c launch.1 /usr/local/man/man1 && \
-chmod 755 build/launch && \
+chmod 755 launch && \
 chmod 644 launch.1 && \
 rm -rf build/launch.build build/intermediates build/.gdb_history && \
 VERSION=`cat VERSION` TARBALL="launch-$VERSION.tar.gz" && \
-DMG="launch-$VERSION.dmg" VOL="launch $VERSION" MOUNTPOINT="/Volumes/$VOL" && \
 cd .. && \
 rm -f launch-$VERSION $TARBALL $DMG && \
 ln -s launch launch-$VERSION && \
 tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history -zchf launch-$VERSION.tar.gz launch-$VERSION && \
-#hdiutil create $DMG -megabytes 5 -ov -type UDIF && \
-#DISK=`hdid $DMG | sed -ne ' /Apple_partition_scheme/ s|^/dev/\([^ ]*\).*$|\1|p'` && \
-#newfs_hfs -v "$VOL" /dev/r${DISK}s2 && \
-#hdiutil eject $DISK && \
-#hdid $DMG && \
-#ditto -rsrc launch "$MOUNTPOINT" && \
-#ditto -rsrc "InstallAnywhere/launch_Web_Installers/InstData/MacOSX/Install launch $VERSION.sit" "/Volumes/launch $VERSION" && \
-#launch "$MOUNTPOINT/Install launch $VERSION.sit" && \
-#./openUp "$MOUNTPOINT" && \
-#sleep 2 && \
-## hdiutil eject $DISK && \ # this doesn't work
-#osascript -e "tell application \"Finder\" to eject disk \"$VOL\"" && \
-#hdiutil convert $DMG -format UDZO -o z$DMG && \
-#mv z$DMG $DMG && \
 scp $TARBALL ainaz:web/nriley/software/ #$DMG 
 :
