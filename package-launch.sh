3012adbb65916d8eaccdda8dbe280ccef4facb63
Nicholas Riley <launchsw@sabi.net>
2006-11-05 12:41:07 +0000

    package-launch.sh: fix install usage to properly create man directory and set permissions on man page; remove mentions of .dmg packaging.

--- package-launch.sh
+++ package-launch.sh
@@ -7,14 +7,15 @@ find . -name \*~ -exec rm '{}' \; && \
 xcodebuild -configuration Deployment clean && \
 xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install && \
 SetFile -c 'R*ch' -t 'TEXT' README VERSION launch.1 && \
-sudo /usr/bin/install -c launch /usr/local/bin && \
-sudo /usr/bin/install -c launch.1 /usr/local/man/man1 && \
+sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1 && \
+sudo /usr/bin/install launch /usr/local/bin && \
+sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1 && \
 chmod 755 launch && \
 chmod 644 launch.1 && \
 VERSION=`cat VERSION` TARBALL="launch-$VERSION.tar.gz" && \
 cd .. && \
-rm -f launch-$VERSION $TARBALL $DMG && \
+rm -f launch-$VERSION $TARBALL && \
 ln -s launch launch-$VERSION && \
 tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history --exclude=build --exclude=\*.mode2 --exclude=\*.pbxuser --exclude=\*.perspective -zchf launch-$VERSION.tar.gz launch-$VERSION && \
-scp $TARBALL ainaz:web/nriley/software/ #$DMG 
+scp $TARBALL ainaz:web/nriley/software/
 :
