deb49504cb2ac9d43469bd19a200792d69f7f2e9
Nicholas Riley <launchsw@sabi.net>
2013-04-26 00:05:45 +0000

    package-launch.sh: Use TextEdit creator and more exclusions.

--- package-launch.sh
+++ package-launch.sh
@@ -6,7 +6,7 @@ cd launch && \
 find . -name \*~ -exec rm '{}' \; && \
 xcodebuild -configuration Deployment clean && \
 xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install && \
-SetFile -c 'R*ch' -t 'TEXT' README VERSION launch.1 && \
+SetFile -c 'ttxt' -t 'TEXT' README VERSION launch.1 && \
 sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1 && \
 sudo /usr/bin/install launch /usr/local/bin && \
 sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1 && \
@@ -16,6 +16,6 @@ VERSION=`cat VERSION` TARBALL="launch-$VERSION.tar.gz" && \
 cd .. && \
 rm -f launch-$VERSION $TARBALL && \
 ln -s launch launch-$VERSION && \
-tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history --exclude=build --exclude=\*.mode2 --exclude=\*.pbxuser --exclude=\*.perspective -zchf launch-$VERSION.tar.gz launch-$VERSION && \
+tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history --exclude=build --exclude=\*.mode* --exclude=\*.pbxuser --exclude=\*.perspective -zchf launch-$VERSION.tar.gz launch-$VERSION && \
 scp $TARBALL ainaz:web/nriley/software/
 :
