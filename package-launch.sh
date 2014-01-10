edf9d3e0715a658f679f1cb4c472148945cb74b2
Nicholas Riley <com-github@sabi.net>
2014-01-09 21:37:03 -0600

    Update packaging script to use bsdtar, osric, zsh -e, new directory layout, etc.

--- package-launch.sh
+++ package-launch.sh
@@ -1,21 +1,25 @@
-#!/bin/zsh
+#!/bin/zsh -e
 
 set -x -v
 
-cd launch && \
-find . -name \*~ -exec rm '{}' \; && \
-xcodebuild -configuration Deployment clean && \
-xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install && \
-SetFile -c 'ttxt' -t 'TEXT' README VERSION launch.1 && \
-sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1 && \
-sudo /usr/bin/install launch /usr/local/bin && \
-sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1 && \
-chmod 755 launch && \
-chmod 644 launch.1 && \
-VERSION=`cat VERSION` TARBALL="launch-$VERSION.tar.gz" && \
-cd .. && \
-rm -f launch-$VERSION $TARBALL && \
-ln -s launch launch-$VERSION && \
-tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history --exclude=build --exclude=\*.mode* --exclude=\*.pbxuser --exclude=\*.perspective -zchf launch-$VERSION.tar.gz launch-$VERSION && \
-scp $TARBALL ainaz:web/nriley/software/
-:
+find . -name \*~ -exec rm '{}' \;
+xcodebuild -configuration Deployment clean
+xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install
+SetFile -c 'ttxt' -t 'TEXT' README VERSION launch.1
+sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1
+sudo /usr/bin/install launch /usr/local/bin
+sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1
+chmod 755 launch
+chmod 644 launch.1
+VERSION=`cat VERSION` TARBALL="launch-$VERSION.tar.gz"
+rm -f ../launch-$VERSION $TARBALL
+dir=$PWD
+ln -s $PWD ../launch-$VERSION
+cd ..
+/usr/bin/tar \
+    --exclude=.DS_Store --exclude=.git\* --exclude=.gdb_history \
+    --exclude=build --exclude=\*.xcworkspace --exclude=xcuserdata \
+    --exclude=launch-\*.tar.gz \
+    -zcLf $dir/launch-$VERSION.tar.gz launch-$VERSION
+rm -f ../launch-$VERSION
+scp $TARBALL osric:web/nriley/software/
