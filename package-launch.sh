9364a03c7f3421593ff7503021df9d201cfb8e37
Nicholas Riley <com-github@sabi.net>
2014-01-12 07:01:52 -0600

    License in separate file.

--- package-launch.sh
+++ package-launch.sh
@@ -5,7 +5,7 @@ set -x -v
 find . -name \*~ -exec rm '{}' \;
 xcodebuild -configuration Deployment clean
 xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install
-SetFile -c 'ttxt' -t 'TEXT' README launch.1
+SetFile -c 'ttxt' -t 'TEXT' README LICENSE launch.1
 sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1
 sudo /usr/bin/install launch /usr/local/bin
 sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1
