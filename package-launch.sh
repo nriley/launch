ac6ba2d10a366157c0c6474adc3abd0f8c1d32a7
Nicholas Riley <com-github@sabi.net>
2017-09-30 19:21:40 -0400

    /usr/local/share/man is preferred over /usr/local/man
    ...in the packaging script, too.
    
    Not sure why we don't need the complex gymnastics that appswitch
    packaging uses, but maybe I'll find out soon!

--- package-launch.sh
+++ package-launch.sh
@@ -6,9 +6,9 @@ find . -name \*~ -exec rm '{}' \;
 xcodebuild -configuration Deployment clean
 xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install
 SetFile -c 'ttxt' -t 'TEXT' README LICENSE launch.1
-sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1
+sudo /usr/bin/install -d -m 755 /usr/local/bin /usr/local/share/man/man1
 sudo /usr/bin/install launch /usr/local/bin
-sudo /usr/bin/install -m 644 launch.1 /usr/local/man/man1
+sudo /usr/bin/install -m 644 launch.1 /usr/local/share/man/man1
 chmod 755 launch
 chmod 644 launch.1
 VERSION=$(agvtool mvers -terse1) TARBALL="$PWD/launch-$VERSION.tar.gz"
